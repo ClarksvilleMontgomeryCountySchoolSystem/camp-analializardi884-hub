18d6fe123d8ed825a7d48414c23d9782f6e062c7
analializardi884-hub <analia.lizardi884@students.cmcss.net>
2026-02-23 11:16:03 -0600

    done

--- task2.py
+++ task2.py
@@ -7,34 +7,33 @@ from data import first, last, birth, days, p_first, p_last, phone, street, city,
 
 
 # first
-
+first = str(input("Enter First Name: "))
 
 # last
-
+last = str(input("Enter Last Name: "))
 
 # birth
-
-
+birth = int(input("Enter Birth Year: "))
 # days
-
+days = int(input("Enter number of days: "))
 
 # p_first
-
+p_first = str(input("Enter Parent's First Name: "))
 
 # p_last
-
+p_last = str(input("Enter Parent's Last Name: "))
 
 # phone
-
+phone = str(input("Enter Phone Number: "))
 
 # street
-
+street = str(input("Enter Street: "))
 
 # city
-
+city = str(input("Enter city: "))
 
 # state
-
+state = str(input("Enter State: "))
 
 # zip_code
-
+zip_code = str(input("Enter Zip Code: "))
